063de6a8680ca87e1f70182437035951ab3f2269
woosujung <wojg58@gmail.com>
2026-01-12 12:40:15 +0900

    chore: payment_history_detail_view? payment_history_view 酉???젣 留덉씠洹몃젅?댁뀡 異붽?

---
+++ supabase/migrations/20260112124004_drop_payment_history_views.sql
@@ -0,0 +1,5 @@
+-- payment_history_detail_view와 payment_history_view 뷰 삭제
+
+DROP VIEW IF EXISTS public.payment_history_detail_view CASCADE;
+DROP VIEW IF EXISTS public.payment_history_view CASCADE;
+
